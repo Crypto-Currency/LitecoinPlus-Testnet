577815cac2a1d20c271a777bc689110b830c806a
Spidersbox <spider@spidersbox.com>
2019-03-28 08:52:46 -0700

    more arm stuff

--- src/version.cpp
+++ src/version.cpp
@@ -11,8 +11,11 @@
 const std::string CLIENT_NAME("LitecoinPlus");
 
 // Client version number
-#define CLIENT_VERSION_SUFFIX   ""
-
+#if defined(__arm__)
+  #define CLIENT_VERSION_SUFFIX   "ARM"
+#else
+  #define CLIENT_VERSION_SUFFIX   ""
+#endif
 
 // The following part of the code determines the CLIENT_BUILD variable.
 // Several mechanisms are used for this:
