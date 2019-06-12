47b1be23c83b91070c22f53dd335cb9874cc3014
typhoonsimon <38272969+typhoonsimon@users.noreply.github.com>
2019-06-12 12:27:41 +0800

    Add files via upload

--- src/init.cpp
+++ src/init.cpp
@@ -344,7 +344,7 @@ void crit_err_hdlr(int sig_num, siginfo_t * info, void * ucontext)
 #elif defined(__x86_64__) // gcc specific
  caller_address = (void *) uc->uc_mcontext.rip; // RIP: x86_64 specific
 #else
-#error Unsupported architecture. // TODO: Add support for other arch.
+ caller_address = NULL;
 #endif
 
  fprintf(stderr, "signal %d (%s), address is %p from %p\n", 
